7f688272eb9649b62ebd805bdd4d2c017dd5b3d7
= <Tiger@cmu.ac.th>
2022-12-09 15:59:19 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
-
 int main(){
+    int N;
+    int Even=0;
+    int Odd=0;
+    
+    do{
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N ;
+    if (N%2 == 0 && N != 0){
+        Even++;
+    }if (N%2 != 0 && N!=0){
+        Odd++;
+    }
+    }
+    while(N != 0);
+    cout <<"#Even numbers = "<<Even <<endl;
+    cout << "#Odd numbers = "<<Odd <<endl;
     return 0;
 }
